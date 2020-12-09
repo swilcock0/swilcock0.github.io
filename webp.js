caca3f72108d9ec1a80be2a2d5375e4918a1c814
Sam W <48917295+swilcock0@users.noreply.github.com>
2020-12-09 19:28:08 +0000

    Responsive images

---
+++ webp.js
@@ -0,0 +1,34 @@
+const imagemin = require("imagemin");
+const imageminWebp = require("imagemin-webp");
+
+const sharp = require("sharp")
+const fs = require('fs');
+const directory = './assets/img/Posts/webp/';
+
+fs.rmdirSync(directory, { recursive: true })
+console.log('Webp directory cleaned!');
+
+imagemin(["./assets/img/Posts/*.{jpg,png}"], {
+  destination: "./assets/img/Posts/webp/",
+  plugins: [
+    imageminWebp({
+      //   quality: 90
+      //   ,
+      //   resize: {
+      //     width: 1000,
+      //     height: 0
+      //   }
+    }),
+  ],
+}).then(() => {
+  console.log("Images Converted Successfully!!!");
+}).then(() => {
+    fs.readdirSync(directory).forEach(file => {
+        sharp(`${directory}/${file}`)
+          .resize(200, 100) // width, height
+          .toFile(`${directory}/${file}-small.webp`);
+        });
+});
+
+
+
